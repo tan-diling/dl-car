095112b5dd6f745a3aa6df2f426d50ef1e22aef0
tan.yw <tan.yw@dealing.tech>
2020-11-20 15:19:07 +0800

    updata login module

--- src/modules/auth/src/controller/auth.service.ts
+++ src/modules/auth/src/controller/auth.service.ts
@@ -1,16 +1,21 @@
+import * as randToken from 'rand-token';
 import { sign } from 'jsonwebtoken';
-
-import { IdentityService } from './identity.service';
-import { Inject, Token, Service } from 'typedi';
-import { IUserToken, IIdentityServiceToken, IIdentityService } from '../interface/login';
 import { JWT_OPTION } from '@app/config';
 import { UnauthorizedError } from 'routing-controllers';
+import { mongoose } from '@typegoose/typegoose';
+import moment = require('moment');
 
 // const jwtSecretOrKey = process.env.JWT_SECRET || "dealing";
 
 const jwtOptions = JWT_OPTION;
-
-export const createJwtToken = (user: IUserToken) => {
+interface IUserToken {
+    id: string;
+    email: string;
+    name: string;
+    role: string;
+    // password?: string;
+}
+const createJwtToken = (user: IUserToken) => {
     const payload = {
         id: user.id,
         email: user.email,
@@ -26,10 +31,88 @@ export const createJwtToken = (user: IUserToken) => {
 
 };
 
-@Service()
+
+class IdentityService {
+    UserModel = mongoose.model<any & mongoose.Document>('User');
+    LoginSessionModel = mongoose.model<any & mongoose.Document>('LoginSession');
+
+    private async clearExpiredLoginSession() {
+        await this.LoginSessionModel.deleteMany({ refreshTime: { $lt: new Date() } }).exec();
+    }
+
+    async userRefreshToken(dto: { user: string, refresh_token: string, skipRefresh?: boolean }) {
+        if (!dto.refresh_token) {
+            throw new UnauthorizedError('refresh_invalid');
+        }
+        const session = await this.LoginSessionModel.findOne({ refreshToken: dto.refresh_token }).exec();
+        if (session == null) {
+            throw new UnauthorizedError('refresh_invalid');
+        }
+        if (new Date() > session.refreshTime) {
+            throw new UnauthorizedError('refresh_expired');
+        }
+        if (String(dto.user) > String(session.user)) {
+            throw new UnauthorizedError('user_invalid');
+        }
+        const user = await this.UserModel.findById(session.user).exec();
+        if (user == null) {
+            throw new UnauthorizedError('user_invalid');
+        }
+        // update session info
+        if (dto.skipRefresh != true) {
+            session.refreshToken = randToken.uid(64);
+            session.accessTime = new Date();
+            await session.save();
+        }
+        return { id: user.id, role: user.role, name: user.name, email: user.email, refresh_token: session.refreshToken };
+    }
+
+    async userLogout(dto: { user: string; device: string; }) {
+
+        if (dto.user == null) {
+            throw new UnauthorizedError('user_invalid')
+        }
+        const session = await this.LoginSessionModel.findOne({ user: dto.user, device: dto.device }).exec();
+        if (session != null) {
+            await session.remove();
+        }
+
+        return { message: "user logout!" };
+    }
+
+    async userLogin(dto: { email: string; password: string; device: string; ip: string; }) {
+        await this.clearExpiredLoginSession();
+        const user = await this.UserModel.findOne({
+            email: { $regex: new RegExp('^' + dto.email + '$', "i") },
+            deleted: false,
+        }).exec();
+        if (user == null) {
+            throw new UnauthorizedError('account_invalid');
+        }
+        if (user.password != dto.password) {
+            throw new UnauthorizedError('password_invalid');
+        }
+        if (!user.isNormal()) {
+            throw new UnauthorizedError('account_forbidden');
+        }
+        // save user session info
+        const loginDto = { device: dto.device, user: user._id };
+        let session = await this.LoginSessionModel.findOne(loginDto).exec();
+        if (session == null) {
+            session = new this.LoginSessionModel(loginDto);
+        }
+        session.accessTime = new Date();
+        session.refreshTime = moment().add(7, 'day').toDate();
+        session.refreshToken = randToken.uid(64);
+        session.ip = dto.ip;
+        await session.save();
+        return { user: { id: user.id, role: user.role, name: user.name, email: user.email }, session };
+    }
+}
+
 export class AuthService {
-    @Inject()
-    service: IdentityService;
+
+    service = new IdentityService();
     /**
      * user login
      * @param dto 
@@ -60,11 +143,28 @@ export class AuthService {
         return await this.service.userLogout(dto);
     }
 
+    async token(dto: { user: string, refresh_token: string }) {
+        // check token is valid
+        const loginUser = await this.service.userRefreshToken(dto);
+
+        // generate access token 
+        const userInfo = {
+            id: loginUser.id,
+            name: loginUser.name,
+            email: loginUser.email,
+            role: loginUser.role,
+        };
+
+        const access_token = createJwtToken(userInfo);
+
+        return { ...userInfo, access_token, refresh_token: loginUser.refresh_token };
+    }
+
     /**
      * user refresh token 
      * @param dto 
      */
-    async refreshToken(dto: { user: string, refresh_token: string }) {
+    async refreshToken(dto: { user: string, refresh_token: string, skipRefresh?: boolean }) {
         // check token is valid
         const loginUser = await this.service.userRefreshToken(dto);
 
@@ -82,5 +182,8 @@ export class AuthService {
     }
 
 }
+
+
+
 
 
--- src/modules/auth/src/controller/login.controller.ts
+++ src/modules/auth/src/controller/login.controller.ts
@@ -1,10 +1,36 @@
 import { Request, Response, NextFunction } from 'express';
 import { JsonController, Post, Get, BodyParam, Body, QueryParams, Req, QueryParam } from 'routing-controllers';
 
-import { LoginDto, RefreshTokenDto, LogoutDto } from './dto/login.dto';
 import * as moment from 'moment';
 import * as requestIp from 'request-ip';
 import { AuthService } from './auth.service'
+import { IsInt, IsMongoId, Min, ValidateNested, IsString, IsOptional, IsIn, IsEmail, IsCreditCard, Matches, IsDateString, IsObject, IsDate, IsNotEmpty } from 'class-validator';
+
+
+class LoginDto {
+    @IsString()
+    device: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    password: string;
+}
+
+class LogoutDto {
+    @IsString()
+    device: string;
+}
+
+class RefreshTokenDto {
+    @IsString()
+    // @IsNotEmpty()
+    refresh_token: string;
+
+}
+
 
 
 @JsonController()
@@ -21,6 +47,14 @@ export class LoginController {
         req.session.token = ret.refresh_token;
         return ret;
 
+    }
+
+    @Post('/token')
+    async token(@Req() req: Request) {
+        const user = req.session?.userId;
+        const refresh_token = req.session?.token;
+        const ret = await this.service.refreshToken({ user, refresh_token, skipRefresh: true });
+        return ret;
     }
 
     @Post('/logout')
